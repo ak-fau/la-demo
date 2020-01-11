088406dc620d6b87c4f9cd4a16a24843aa43e0c7
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2020-01-12 00:03:12 +0100

    Make use of Altera/Intel library for Virtual JTAG (instead of a local component description)

--- mx10.vhd
+++ mx10.vhd
@@ -1,6 +1,9 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+library altera_mf;
+use altera_mf.altera_mf_components.all;
+
 entity mx10 is
   port(
     clk25        : in    std_logic;
@@ -43,40 +46,6 @@ architecture top of mx10 is
   signal dr_s : std_logic_vector(7 downto 0);
   signal dr : std_logic_vector(7 downto 0);
 
-  component v_jtag is
-    port (
-      tck                : out std_logic;
-      tms                : out std_logic;
-      tdi                : out std_logic;
-      tdo                : in  std_logic := 'X';
-      ir_in              : out std_logic_vector(VJTAG_IR_LEN-1 downto 0);
-      ir_out             : in  std_logic_vector(VJTAG_IR_LEN-1 downto 0) := (others => 'X');
-      virtual_state_cdr  : out std_logic;
-      virtual_state_sdr  : out std_logic;
-      virtual_state_e1dr : out std_logic;
-      virtual_state_pdr  : out std_logic;
-      virtual_state_e2dr : out std_logic;
-      virtual_state_udr  : out std_logic;
-      virtual_state_cir  : out std_logic;
-      virtual_state_uir  : out std_logic;
-      jtag_state_tlr     : out std_logic;
-      jtag_state_rti     : out std_logic;
-      jtag_state_sdrs    : out std_logic;
-      jtag_state_cdr     : out std_logic;
-      jtag_state_sdr     : out std_logic;
-      jtag_state_e1dr    : out std_logic;
-      jtag_state_pdr     : out std_logic;
-      jtag_state_e2dr    : out std_logic;
-      jtag_state_udr     : out std_logic;
-      jtag_state_sirs    : out std_logic;
-      jtag_state_cir     : out std_logic;
-      jtag_state_sir     : out std_logic;
-      jtag_state_e1ir    : out std_logic;
-      jtag_state_pir     : out std_logic;
-      jtag_state_e2ir    : out std_logic;
-      jtag_state_uir     : out std_logic);
-  end component v_jtag;
-
 begin
 
   clk <= clk25;
@@ -114,7 +83,11 @@ begin
   led(0) <= not button(0);
   led(1) <= not button(1);
 
-  u0 : component v_jtag
+  u0 : component sld_virtual_jtag
+   generic map (
+      sld_auto_instance_index => "NO",
+      sld_instance_index => 1,
+      sld_ir_width => 1)
     port map (
       tck                => jtag_tck,
       tms                => jtag_tms,
