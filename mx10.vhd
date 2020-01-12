527af482ae35a5a9d2252a822d3bea6cabf3628a
Anton Kuzmin <anton.kuzmin@cs.fau.de>
2020-01-12 02:03:03 +0100

    Move all Virtual JTAG parts into a separate file

--- mx10.vhd
+++ mx10.vhd
@@ -1,9 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-library altera_mf;
-use altera_mf.altera_mf_components.all;
-
 entity mx10 is
   port(
     clk25        : in    std_logic;
@@ -32,19 +29,11 @@ entity mx10 is
 end mx10;
 
 architecture top of mx10 is
-  constant VJTAG_IR_LEN : integer := 1;
 
   signal clk, reset : std_logic;
   signal rst_n : std_logic_vector(4 downto 0) := (others => '0');
 
-  signal jtag_tck, jtag_tms, jtag_tdi, jtag_tdo : std_logic;
-  signal jtag_ir_in  : std_logic_vector(VJTAG_IR_LEN-1 downto 0);
-  signal jtag_ir_out : std_logic_vector(VJTAG_IR_LEN-1 downto 0);
-
-  signal jtag_tlr : std_logic;
-  signal v_cdr, v_sdr, v_udr : std_logic;
-  signal dr_s : std_logic_vector(7 downto 0);
-  signal dr : std_logic_vector(7 downto 0);
+  signal pmod_led8 : std_logic_vector(7 downto 0);
 
 begin
 
@@ -72,7 +61,7 @@ begin
   scl <= 'Z';
   sda <= 'Z';
 
-  pmod_J2 <= dr;
+  pmod_J2 <= pmod_led8;
   pmod_J3 <= (others => 'Z');
   pmod_J4 <= (others => 'Z');
   pmod_J5 <= (others => 'Z');
@@ -83,75 +72,10 @@ begin
   led(0) <= not button(0);
   led(1) <= not button(1);
 
-  u0 : component sld_virtual_jtag
-   generic map (
-      sld_auto_instance_index => "NO",
-      sld_instance_index => 1,
-      sld_ir_width => 1)
+  u0: entity work.vjtag
     port map (
-      tck                => jtag_tck,
-      tms                => jtag_tms,
-      tdi                => jtag_tdi,
-      tdo                => jtag_tdo,
-      ir_in              => jtag_ir_in,
-      ir_out             => jtag_ir_out,
-      virtual_state_cdr  => v_cdr,
-      virtual_state_sdr  => v_sdr,
-      virtual_state_e1dr => open,
-      virtual_state_pdr  => open,
-      virtual_state_e2dr => open,
-      virtual_state_udr  => v_udr,
-      virtual_state_cir  => open,
-      virtual_state_uir  => open,
-      jtag_state_tlr     => jtag_tlr,
-      jtag_state_rti     => open,
-      jtag_state_sdrs    => open,
-      jtag_state_cdr     => open,
-      jtag_state_sdr     => open,
-      jtag_state_e1dr    => open,
-      jtag_state_pdr     => open,
-      jtag_state_e2dr    => open,
-      jtag_state_udr     => open,
-      jtag_state_sirs    => open,
-      jtag_state_cir     => open,
-      jtag_state_sir     => open,
-      jtag_state_e1ir    => open,
-      jtag_state_pir     => open,
-      jtag_state_e2ir    => open,
-      jtag_state_uir     => open);
-
-  -- jtag_tdo <= jtag_tdi;
-  jtag_ir_out <= (others => '0');
-
-  led_green <= jtag_ir_in(0);
-
-  process (jtag_tck, reset)
-  begin
-    if reset = '1' then
-      dr <= (others => '0');
-    elsif falling_edge(jtag_tck) then
-      if v_udr = '1' then
-        dr <= dr_s;
-      end if;
-    end if;
-  end process;
-
-  process (jtag_tck, reset)
-  begin
-    if reset = '1' then
-      dr_s <= (others => '0');
-    elsif rising_edge(jtag_tck) then
-      if jtag_tlr = '1' then
-        dr_s <= (others => '0');
-      elsif v_cdr = '1' then
-        dr_s <= x"C" & "000" & jtag_ir_in(0);
-      elsif v_sdr = '1' then
-        dr_s(dr_s'left-1 downto 0) <= dr_s(dr_s'left downto 1);
-        dr_s(dr_s'left) <= jtag_tdi;
-      end if;
-    end if;
-  end process;
-
-  jtag_tdo <= dr_s(0);
+      reset => reset,
+      ir0   => led_green,
+      data  => pmod_led8);
 
 end top;
---
+++ vjtag.vhd
@@ -0,0 +1,99 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+library altera_mf;
+use altera_mf.altera_mf_components.all;
+
+entity vjtag is
+  port (
+    reset : in std_logic;
+    ir0 : out std_logic;
+    data: out std_logic_vector(7 downto 0));
+end entity vjtag;
+
+architecture rtl of vjtag is
+  constant VJTAG_IR_LEN : integer := 1;
+
+  signal jtag_tck, jtag_tms, jtag_tdi, jtag_tdo : std_logic;
+  signal jtag_ir_in  : std_logic_vector(VJTAG_IR_LEN-1 downto 0);
+  signal jtag_ir_out : std_logic_vector(VJTAG_IR_LEN-1 downto 0);
+
+  signal jtag_tlr : std_logic;
+  signal v_cdr, v_sdr, v_udr : std_logic;
+  signal dr_s : std_logic_vector(7 downto 0);
+  signal dr : std_logic_vector(7 downto 0);
+
+begin
+
+  ir0 <= jtag_ir_in(0);
+  data <= dr;
+
+  u0 : component sld_virtual_jtag
+   generic map (
+      sld_auto_instance_index => "NO",
+      sld_instance_index => 1,
+      sld_ir_width => 1)
+    port map (
+      tck                => jtag_tck,
+      tms                => jtag_tms,
+      tdi                => jtag_tdi,
+      tdo                => jtag_tdo,
+      ir_in              => jtag_ir_in,
+      ir_out             => jtag_ir_out,
+      virtual_state_cdr  => v_cdr,
+      virtual_state_sdr  => v_sdr,
+      virtual_state_e1dr => open,
+      virtual_state_pdr  => open,
+      virtual_state_e2dr => open,
+      virtual_state_udr  => v_udr,
+      virtual_state_cir  => open,
+      virtual_state_uir  => open,
+      jtag_state_tlr     => jtag_tlr,
+      jtag_state_rti     => open,
+      jtag_state_sdrs    => open,
+      jtag_state_cdr     => open,
+      jtag_state_sdr     => open,
+      jtag_state_e1dr    => open,
+      jtag_state_pdr     => open,
+      jtag_state_e2dr    => open,
+      jtag_state_udr     => open,
+      jtag_state_sirs    => open,
+      jtag_state_cir     => open,
+      jtag_state_sir     => open,
+      jtag_state_e1ir    => open,
+      jtag_state_pir     => open,
+      jtag_state_e2ir    => open,
+      jtag_state_uir     => open);
+
+  jtag_ir_out <= (others => '0');
+
+  process (jtag_tck, reset)
+  begin
+    if reset = '1' then
+      dr <= (others => '0');
+    elsif falling_edge(jtag_tck) then
+      if v_udr = '1' then
+        dr <= dr_s;
+      end if;
+    end if;
+  end process;
+
+  process (jtag_tck, reset)
+  begin
+    if reset = '1' then
+      dr_s <= (others => '0');
+    elsif rising_edge(jtag_tck) then
+      if jtag_tlr = '1' then
+        dr_s <= (others => '0');
+      elsif v_cdr = '1' then
+        dr_s <= x"C" & "000" & jtag_ir_in(0);
+      elsif v_sdr = '1' then
+        dr_s(dr_s'left-1 downto 0) <= dr_s(dr_s'left downto 1);
+        dr_s(dr_s'left) <= jtag_tdi;
+      end if;
+    end if;
+  end process;
+
+  jtag_tdo <= dr_s(0);
+
+end architecture rtl;
